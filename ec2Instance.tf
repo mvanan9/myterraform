96b6dabc83b5ebb270f9c585bedafd37768bf6db
mvanan9 <mvanan9@gmail.com>
2025-12-03 23:22:42 +0530

    Create ec2Instance.tf

---
+++ ec2Instance.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+region="us-east-1"
+}
+resource "aws_instance" "myfirstec2" {
+  ami           = 	ami-0c9ec63dfdeb08987
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
